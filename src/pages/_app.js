98ee6aad34f4a1945212f17f71e5949c344ca833
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-04-09 22:52:00 -0400

    added tailwing config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import { Provider } from "react-redux";
 import { useStore } from "../redux/store";
 // styles
-import "../styles/globals.css";
-import "tailwindcss/tailwind.css";
+// import "../styles/globals.css";
+// import "tailwindcss/tailwind.css";
+
+import "../styles/tailwind.css"
+
 
 const App = ({ Component, pageProps }) => {
   const store = useStore(pageProps.initialReduxState);
