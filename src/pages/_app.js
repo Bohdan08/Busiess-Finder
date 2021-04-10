8c074d752f512508449171f22f056d529b32e9ec
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-04-09 22:54:47 -0400

    uncommented values

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,8 @@
 import { Provider } from "react-redux";
 import { useStore } from "../redux/store";
+
 // styles
-// import "../styles/globals.css";
-// import "tailwindcss/tailwind.css";
-
-import "../styles/tailwind.css"
-
+import "../styles/tailwind.css";
 
 const App = ({ Component, pageProps }) => {
   const store = useStore(pageProps.initialReduxState);
